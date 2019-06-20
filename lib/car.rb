43336852cbca34d933c28de2e23a4b1263623f2d
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-06-20 01:35:15 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,13 @@
 class Vehicle
   attr_accessor 
   :wheel_size, :wheel_number
+end
   
   def 
     initialize(wheel_size, whee_number)
+    @wheel_size = wheel_size
+    
+    @weel_number 
     
     
   
